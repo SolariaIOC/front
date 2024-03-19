3f522e26e66e5fb22499e8fba68116c345b99200
Gonzalo Scarpa <gschecho@gmail.com>
2024-03-19 20:57:35 +0100

    eliminando archivos de React

--- assets/dist/js/main.js
+++ assets/dist/js/main.js
@@ -1,4 +1,7 @@
 import { renderizaFragmento } from "./utils/modularizacionHtml.js";
+import { renderComponent } from "./utils/components.js";
+import { decodificaJWT } from "./utils/token.js";
+import { hideNav } from "./utils/hideNav.js";
 
 renderizaFragmento("#barraDeNavegacion","./components/nav.html");
 renderizaFragmento("#footer","./components/footer.html");
@@ -6,16 +9,33 @@ renderizaFragmento("#footer","./components/footer.html");
 
 renderizaFragmento("#inmobles","./components/inmobles.html");
 renderizaFragmento("#promo","./components/promo.html");
-renderizaFragmento("#loginModal","./components/login-modal-html");
+renderizaFragmento("#loginModal","./components/login-modal.html");
 
 
 
-const token = true
+
+
+
+
+
+hideNav()
+
+
+renderComponent("#banner", "/components/banner.html")
+
+let token = getCookie("token");
+let usuario = decodificaJWT(token); 
+
+
 
 if(token){
      renderizaFragmento("#banner","./components/banner.html");
 }
 
 
+
+
+
+console.log( decodificaJWT(token) )
 
 
