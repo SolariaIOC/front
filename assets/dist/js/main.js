d96600a5a855363109ff2625eb3950bdc0f12e63
Gonzalo Scarpa <gschecho@gmail.com>
2024-03-19 21:27:00 +0100

    arreglada barra de navegación

--- assets/dist/js/main.js
+++ assets/dist/js/main.js
@@ -3,6 +3,14 @@ import { renderComponent } from "./utils/components.js";
 import { decodificaJWT } from "./utils/token.js";
 import { hideNav } from "./utils/hideNav.js";
 
+
+
+
+/*Animacion de la barra de navegación*/
+hideNav()
+
+
+
 renderizaFragmento("#barraDeNavegacion","./components/nav.html");
 renderizaFragmento("#footer","./components/footer.html");
 
@@ -18,7 +26,7 @@ renderizaFragmento("#loginModal","./components/login-modal.html");
 
 
 
-hideNav()
+
 
 
 renderComponent("#banner", "/components/banner.html")
@@ -26,7 +34,12 @@ renderComponent("#banner", "/components/banner.html")
 let token = getCookie("token");
 let usuario = decodificaJWT(token); 
 
+localStorage.setItem('usuario', JSON.stringify(usuario));
 
+if (usuario){
+   let banner = document.querySelector('#banner');
+     banner.textContent = "Benvingut, " + usuario.Nom;
+}
 
 if(token){
      renderizaFragmento("#banner","./components/banner.html");
