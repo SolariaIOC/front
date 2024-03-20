3414d850be569d523cd02672f7e0692577435ea8
Dani Roman <daniel.roman@thehiddenpanda.com>
2024-03-20 22:41:07 +0100

    Test Token solaria.website

--- assets/dist/js/utils/token.js
+++ assets/dist/js/utils/token.js
@@ -1,6 +1,9 @@
 export function decodificaJWT(token) {
 
   const arrayToken = token.split('.');
+  console.log("Array Token: ");
+  console.log(arrayToken);
+
   const tokenPayload = JSON.parse(atob(arrayToken[1]));
   //console.log(tokenPayload)
   return tokenPayload
