a5cb2a95c5e5a0805dc4c1608df45e01fabae76a
Gon <gschecho@gmail.com>
2024-03-21 08:40:35 +0100

    nuevo registro

--- assets/dist/js/main.js
+++ assets/dist/js/main.js
@@ -1,5 +1,4 @@
 import { renderizaFragmento } from "./utils/modularizacionHtml.js";
-import { renderComponent } from "./utils/components.js";
 import { decodificaJWT } from "./utils/token.js";
 import { hideNav } from "./utils/hideNav.js";
 
@@ -14,21 +13,29 @@ hideNav()
 
 /* RENDERIZACIÓN DE ELEMENTOS COMPARTIDOS */
 
-renderizaFragmento("#barraDeNavegacion","./components/nav.html");
+renderizaFragmento("#barra-navegacion","./components/nav.html");
 renderizaFragmento("#footer","./components/footer.html");    
 
 /* RENDERIZACIÓN ESPECIFICA */
 
 let ruta =  window.location.href;
-if(ruta.includes("registre")){
 
-}
 
 if(ruta.includes("index")){
      renderizaFragmento("#inmobles","./components/inmobles.html");
      renderizaFragmento("#promo","./components/promo.html");
-     renderizaFragmento("#loginModal","./components/login-modal.html");    
+     renderizaFragmento("#login-modal","./components/login-modal.html");    
 }
+
+if(ruta.includes("registre")){ 
+    renderizaFragmento("#login-modal","./components/login-modal.html");  
+  
+    renderizaFragmento("#registre-form","./components/registre-form.html");
+
+   
+}
+
+
 
 //REDIRIGE A
 //window.location.assign()
@@ -53,25 +60,3 @@ if(token !== undefined || token !=='') {
           console.log( decodificaJWT(token) )
      }
 }
-
-/*
-function getCookies(name) {
-     const cookies = document.cookie.split(';'); // Divide la cadena de cookies en un arreglo
-     for (let i = 0; i < cookies.length; i++) {
-         const cookie = cookies[i].trim(); // Elimina espacios en blanco al principio y al final de la cookie
-         // Verifica si la cookie comienza con el nombre buscado
-         if (cookie.startsWith(name + '=')) {
-             return cookie.substring(name.length + 1); // Retorna el valor de la cookie, omitiendo el nombre y el signo '='
-         }
-     }
-     return null; // Retorna null si no se encontró la cookie
- }
- 
- // Ejemplo de uso:
- const token = getCookie('token');
- if (token) {
-     console.log('El valor de la cookie "token" es:', token);
- } else {
-     console.log('No se encontró la cookie "token"');
- }
- */
