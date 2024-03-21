6245c8c53f7665be1f112c00c10c99b8b87a0f85
Gonzalo Scarpa <gschecho@gmail.com>
2024-03-21 01:51:04 +0100

    añadido registro

--- assets/dist/js/main.js
+++ assets/dist/js/main.js
@@ -1,9 +1,11 @@
 import { renderizaFragmento } from "./utils/modularizacionHtml.js";
-import { renderComponent } from "./utils/components.js";
+
 import { decodificaJWT } from "./utils/token.js";
 import { hideNav } from "./utils/hideNav.js";
+import { registro } from "./registre.js";
 
 
+const mainContent = document.querySelector('main-content');
 
 /* FUNCIONES GENERALES */
 
@@ -12,69 +14,56 @@ hideNav()
 
 /* RENDERIZACIÓN DE ELEMENTOS COMPARTIDOS */
 
-renderizaFragmento("#barraDeNavegacion","./components/nav.html");
+renderizaFragmento("#barra-navegacion","./components/nav.html");
 renderizaFragmento("#footer","./components/footer.html");    
 
 /* RENDERIZACIÓN ESPECIFICA */
 
-let ruta =  window.location.href;
-if(ruta.includes("registre")){
-    
-
-}
+const ruta = window.location.href;
 
 if(ruta.includes("index")){
 
      renderizaFragmento("#inmobles","./components/inmobles.html");
      renderizaFragmento("#promo","./components/promo.html");
-     renderizaFragmento("#loginModal","./components/login-modal.html");    
+     renderizaFragmento("#login-modal","./components/login-modal.html");    
+}
+
+if(ruta.includes("registre")){ 
+    renderizaFragmento("#login-modal","./components/login-modal.html");  
+  
+    renderizaFragmento("#registre-form","./components/registre-form.html");
+
+   
 }
 
 
 //REDIRIGE A
 //window.location.assign()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 let token = getCookie("token");
-let usuario = decodificaJWT(token); 
 
-localStorage.setItem('usuario', JSON.stringify(usuario));
+if(token !== undefined || token !=='') {
+
+     let usuario = decodificaJWT(token);
+
+     console.log(usuario);
+
+     if(usuario !== null) {
+          localStorage.setItem('usuario', JSON.stringify(usuario));
+
+          const usuarioLocal = JSON.parse(localStorage.getItem('usuario'));
+
+          console.log('usuario en local storage')
+          console.log(usuarioLocal);
+
+          console.log(usuario)
+          console.log( decodificaJWT(token) )
+     }
+}
 
 
-const usuarioLocal = JSON.parse(localStorage.getItem('usuario'));
-
-console.log('usuario en local storage')
-console.log(usuarioLocal)
 
 
-
-
-
-
-
-
-console.log(usuario)
-
-
-
-
-console.log( decodificaJWT(token) )
 
 
 
