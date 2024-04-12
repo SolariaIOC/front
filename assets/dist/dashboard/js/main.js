aa2fe3a02602e8a2c2fb7e60639c4f397f4aaae7
Dani Roman <daniel.roman@thehiddenpanda.com>
2024-04-12 19:09:57 +0900

    Make dashboard dynamic

--- assets/dist/dashboard/js/main.js
+++ assets/dist/dashboard/js/main.js
@@ -1,9 +1,35 @@
 import { renderizaFragmento } from "../../js/utils/modularizacionHtml.js";
 
 // Renders
-renderizaFragmento("#sidebar", "../../components/dashboard/dashboard_menu.html");
-renderizaFragmento("#top-bar", "../../components/dashboard/dashboard_top_bar.html");
-renderizaFragmento("#footer", "../../components/dashboard/dashboard_footer.html");
+renderizaFragmento("#sidebar", "../../components/dashboard/dashboard_layout/menu.html");
+renderizaFragmento("#top-bar", "../../components/dashboard/dashboard_layout/top_bar.html");
+renderizaFragmento("#footer", "../../components/dashboard/dashboard_layout/footer.html");
 
-// TODO: Make it dynamic
-renderizaFragmento("#page-content", "../../components/dashboard/dashboard_llistat_immobles.html");
+// Gets the url and render the content of this url
+renderizaFragmento("#page-content", "../../components/dashboard/"+getHtmlFromUrl(window.location.hash));
+
+window.onload = function() {
+    let elements = document.getElementsByClassName("dashboard-link");
+    for(let i = 0; i < elements.length; i++) {
+        elements[i].onclick = function () {
+            // Renders the content from the link
+            renderizaFragmento("#page-content", "../../components/dashboard/"+getHtmlFromUrl(elements[i].href));
+        }
+    }
+};
+
+function getHtmlFromUrl(url){
+    let urlPath = url.split('?')[0].split('#')[1];
+    switch (urlPath) {
+        case 'usuaris':
+            return 'usuaris/llistat_usuaris.html';
+        case 'editar-immoble':
+            return 'immobles/editar_immoble.html';
+        case 'crear-immoble':
+            return 'immobles/crear_immoble.html';
+        case 'immobles':
+        default:
+            return 'immobles/llistat_immobles.html';
+    }
+}
+
